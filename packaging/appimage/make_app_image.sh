c35232d7c499a2591b3b2b873c8a3b7d096852d4
probonopd <probonopd@users.noreply.github.com>
2018-08-26 09:24:27 +0000

    Update make_app_image.sh

--- packaging/appimage/make_app_image.sh
+++ packaging/appimage/make_app_image.sh
@@ -17,7 +17,7 @@ conda create -n libertem python=3.6 -y || exit 1
 # FIXME: install specific version (for example from pypi, or continuous build, ...)n s
 
 # Build wheel
-python "$BASE_DIR"/setup.py bdist_wheel
+( cd "$BASE_DIR" ; python setup.py bdist_wheel )
 
 pip install "$BASE_DIR"/dist/*.whl || exit 1
 
